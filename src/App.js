3b0947689a6ff13ffbb44e6acad14bde7d5c10d4
Samu <samu.rinne@metropolia.fi>
2020-09-24 13:00:12 +0300

    Add working routing

--- src/App.js
+++ src/App.js
@@ -9,13 +9,29 @@ import L from 'leaflet'
 const request = require('request');
 
 function App() {
-  let mapContainer=false;
   let weather = conditions();
   let newMap = null;
-  let mapReady = null;
+  let fromSet = false;
+  let whereSet = false;
+  let routingControl;
+  const [options, setOptions] = useState([]);
+  let whereBox = <form>
+    <table>
+      <tbody>
+      <tr>
+        <td>
+          <label>Minne</label>
+          <select id='selectValue' onChange={drawRoute}>
+            {options}
+          </select>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </form>
   const[stations, setStations] = useState([]);
-  const[from, setFrom] = useState([null, null]);
-  const [where, setWhere] = useState([null, null]);
+  let from = null;
+  let where = null;
   const position = [60.192059, 24.945831]
   useEffect(() => {
     newMap = L.map('map', {
@@ -25,67 +41,80 @@ function App() {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
     }).addTo(newMap);
-
-
-
-    /*L.Routing.control({
-      waypoints: [
-        L.latLng(from),
-        L.latLng(where)
-      ],
-      routeWhileDragging: true
-    }).addTo(newMap);
-    mapReady = true;*/
   }, [])
-
-  /*const map = (
-      <Map center={position} zoom={12} style={{ height: '1080px', width: '1920px' }}>
-        <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        {stations.map(station => {
-
-          const ensemble = [station.lat, station.lon];
-          return (
-              <Marker position={ensemble} key={station.id}>
-                <Popup>
-                  Aseman nimi: {station.name}<br/>
-                  Pyöriä vapaapaana: {station.bikesAvailable} <br/>
-                  Paikkoja jäljellä: {station.spacesAvailable}
-                </Popup>
-              </Marker>
-            )
-          })
-        }
-      </Map>
-  )*/
 
   useEffect(() => {
     request (reqStation, false, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         setStations((JSON.parse(JSON.stringify(body.data.bikeRentalStations, null, 4))));
         JSON.parse(JSON.stringify(body.data.bikeRentalStations, null, 4)).forEach(function(entry) {
-          L.marker([entry.lat, entry.lon]).bindPopup("Aseman nimi:" + entry.name + " <br>Pyöriä vapaapaana: " + entry.bikesAvailable + " <br>Paikkoja jäljellä: " + entry.spacesAvailable).addTo(newMap);
+          L.marker([entry.lat, entry.lon])
+              .bindPopup("Aseman nimi:" + entry.name + " <br>Pyöriä vapaapaana: " + entry.bikesAvailable + " <br>Paikkoja jäljellä: " + entry.spacesAvailable).on('click', updateFrom)
+              .addTo(newMap);
         })
       }
     });
      }, [newMap]);
-  /*useEffect( () => {
-    console.log("KOLMAS NEW MAP", newMap, stations);
-      stations.forEach(function(entry) {
-        console.log(entry.lat, entry.lon);
-        L.marker([entry.lat, entry.lon]).addTo(newMap);
-      })
 
-  }, []);*/
+  useEffect( () => {
+    let temp = [];
+    stations.forEach(function(entry){
+      console.log(entry.name);
+      temp.push(<option key={entry.name} value={[entry.lat + " " + entry.lon]}>{entry.name}</option>)
+    })
+    setOptions(temp);
+  }, [stations])
+
+  function drawRoute(){
+    let selection = document.getElementById('selectValue').value;
+    let where = [selection.split(" ")[0], selection.split(" ")[1]];
+    console.log(from, where)
+    if (from && where){
+      if (routingControl != null) {
+        newMap.removeControl(routingControl);
+        routingControl = null;
+      }
+      console.log("Uusi route")
+      routingControl = L.Routing.control({
+        waypoints: [
+          L.latLng(from),
+          L.latLng(where)
+        ],
+        routeWhileDragging: true,
+      }).addTo(newMap);
+    }
+  }
+  function updateWhere(e){
+    console.log("TÄNNE");
+    where = [e.target._latlng.lat, e.target._latlng.lng];
+    whereSet = true;
+  }
+  function updateFrom(e){
+    console.log(from);
+    if (!fromSet){
+      console.log("EKA")
+    }
+    if (fromSet){
+      console.log("ASDSDFASDF")
+    }
+    from = [e.target._latlng.lat, e.target._latlng.lng];
+    console.log(fromSet)
+    fromSet = true;
+    console.log(fromSet)
+    drawRoute();
+  }
   return (
     <div className="App">
       <div id="map" className="Map" >
         {newMap}
       </div>
-      <div className="Weather">
-        {weather}
+      <div className='rightSide'>
+        <div className="Weather">
+          {weather}
+        </div>
+        <div className="Where">
+          {whereBox}
+        </div>
       </div>
     </div>
   );
